6ce07548a8b3bfc693975146f14a1092831c9795
Sepha <sephanyaboke@gmail.com>
2026-06-23 15:04:05 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(name, price){
+
+cart.push({
+name:name,
+price:price
+});
+
+localStorage.setItem("cart",
+JSON.stringify(cart));
+
+alert(name + " added to cart");
+}
